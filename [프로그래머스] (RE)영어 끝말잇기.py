3d92ba1447e1c2527a08eb0890403a5840a446a3
yurim022 <45115557+yurim022@users.noreply.github.com>
2020-12-14 11:08:30 +0900

    Update and rename [프로그래머스] 영어 끝말잇기.py to [프로그래머스] (RE)영어 끝말잇기.py
    다른분 풀이 더 깔끔한거 참고!

--- [프로그래머스] (RE)영어 끝말잇기.py
+++ [프로그래머스] (RE)영어 끝말잇기.py
@@ -14,3 +14,13 @@ def solution(n, words):
         lastWord = word[-1]
 
     return answer
+
+
+#다른분 풀이
+#index 0부터 시작하면 더 깔끔하게 정리 가능
+#python indexing으로 별개의 변수 선언하지 않고 바로 처리
+def solution(n, words):
+    for p in range(1, len(words)):
+        if words[p][0] != words[p-1][-1] or words[p] in words[:p]: return [(p%n)+1, (p//n)+1]
+    else:
+        return [0,0]
