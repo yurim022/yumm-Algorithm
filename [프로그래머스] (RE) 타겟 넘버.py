d349477fb393adfb170daac9fd6925459a14b0e6
yurim022 <45115557+yurim022@users.noreply.github.com>
2020-12-16 14:58:31 +0900

    Update [프로그래머스] (RE) 타겟 넘버.py
    dfs추가

--- [프로그래머스] (RE) 타겟 넘버.py
+++ [프로그래머스] (RE) 타겟 넘버.py
@@ -4,3 +4,12 @@ def solution(numbers, target):
     l = [(x,-x) for x in numbers]
     s = list(map(sum,product(*l)))
     return s.count(target)
+
+#원래 의도에 맞게 DFS
+def solution(numbers, target):
+    if not numbers and target == 0:
+        return 1
+    elif not numbers:
+        return 0
+    else:
+        return solution(numbers[1:],target+numbers[0])+ solution(numbers[1:],target-numbers[0])
