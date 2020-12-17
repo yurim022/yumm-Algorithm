114e116221a6be5f2d11fcacf8e1f415da00bcbe
yurim022 <45115557+yurim022@users.noreply.github.com>
2020-12-17 11:00:10 +0900

    Create [프로그래머스] 완주하지 못한 선수.java

---
+++ [프로그래머스] 완주하지 못한 선수.java
@@ -0,0 +1,14 @@
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Arrays;
+
+class Solution{
+    public String solution(String[] participant, String[] completion){
+        Map<String,Integer> find = new HashMap<>();
+        for (String part: participant) find.put(part,find.getOrDefault(part,0)+1);
+        for (String comp: completion) find.put(comp,find.get(comp)-1);
+        for (String key : find.keySet()) 
+            if (find.get(key) != 0) return key;
+        return "None";
+    }    
+}
