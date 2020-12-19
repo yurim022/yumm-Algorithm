d5470b71abf9e3ac741eef6591681ed0299b4d06
yurim022 <45115557+yurim022@users.noreply.github.com>
2020-12-19 17:46:05 +0900

    Create [프로그래머스] (RE) 여행경로.java
    다시 풀어보기!

---
+++ [프로그래머스] (RE) 여행경로.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.Collections;
+ 
+public class Solution {
+    boolean[] visited;
+    ArrayList<String> answers;
+ 
+    public String[] solution(String[][] tickets) {
+        visited = new boolean[tickets.length];
+        answers = new ArrayList<String>();
+        int count = 0;
+        dfs(count,"ICN","ICN",tickets);
+        Collections.sort(answers);
+        String[] answer = answers.get(0).split(" ");
+        return answer;
+    }
+    public void dfs(int count, String present, String answer, String[][]tickets) {
+        if(count == tickets.length){
+            answers.add(answer);
+            return;
+        }
+        for (int i = 0; i <tickets.length;i++){
+            if(!visited[i] && tickets[i][0].equals(present)){
+               visited[i] = true;
+               dfs(count+1,tickets[i][1],answer+" "+tickets[i][1], tickets);
+               visited[i] = false;
+            }
+        }
+   
+    }
+}
