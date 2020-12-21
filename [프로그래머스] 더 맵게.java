dac70d7302f312ce9c8592c17d87174a1db78acf
yurim cho <45115557+yurim022@users.noreply.github.com>
2020-12-21 21:56:47 +0900

    Create [프로그래머스] 더 맵게.java
    PriorityQueue -> Offer, Peek, Poll

---
+++ [프로그래머스] 더 맵게.java
@@ -0,0 +1,21 @@
+import java.util.PriorityQueue;
+
+class Solution {
+    public int solution(int[] scoville, int K) {
+        //일반적으로 python이 빠르나, 효율성 테스트에서는 java가 이김
+        int cnt = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue();
+        
+        //우선순위 큐에 파일 넣기
+        for (int s:scoville){
+            pq.offer(s);
+        }
+        //K보다 커질때까지 연산
+        while(pq.peek()<K){
+            if(pq.size()==1){return -1;}
+            pq.offer(pq.poll()+pq.poll()*2);
+            cnt ++;
+        }
+        return cnt;
+    }
+}
