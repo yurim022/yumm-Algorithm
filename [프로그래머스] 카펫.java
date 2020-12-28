0540d172ccfb875bf0eda86b4509d96e2bf1ba4c
yurim cho <45115557+yurim022@users.noreply.github.com>
2020-12-28 16:20:28 +0900

    Create [프로그래머스] 카펫.java

---
+++ [프로그래머스] 카펫.java
@@ -0,0 +1,25 @@
+import java.lang.*;
+
+class Solution {
+    public int[] solution(int brown, int red) {
+        int mul = brown +red;
+        for(int i = 3; i<= Math.sqrt(mul); i++){
+            if(mul%i == 0){
+                int col = i; //세로
+                int row = mul/i; //가로
+                if(((row+col)*2-4) == brown){
+                    int[] answer = new int[2];
+                    answer[0] = row;
+                    answer[1] = col;
+                    return answer;
+                }
+            }
+        }
+        
+        int[] nop = new int[2];
+        nop[0] = 0;
+        nop[1] = 0;
+        return nop; 
+
+    }
+}
