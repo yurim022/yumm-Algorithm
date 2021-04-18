812246d6fc1d18c0193515f30beb838fbb9939be
yurim cho <45115557+yurim022@users.noreply.github.com>
2021-04-18 16:19:07 +0900

    Create [프로그래머스] (RE) 점프와순간이동.java
    top-down 사용, 
    dfs를 사용하면 stackoverflow 발생

---
+++ [프로그래머스] (RE) 점프와순간이동.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+    public int solution(int n) {
+        //top-down 방식의 처리, n이 짝수이면 /2로 순간이동 하고 홀수일 경우 점프
+        int usedCount = 0;
+        
+        while ( n > 0) {
+            if (n % 2 == 0){ //순간이동
+                n /= 2;
+                continue;
+            }
+            
+            //점프
+            n -= 1;
+            usedCount += 1;
+        }
+        
+        return usedCount;
+    }
+}
