4ae91473ccc2a1dc03548f81e3f753a6967013e7
yurim cho <45115557+yurim022@users.noreply.github.com>
2021-05-15 22:41:18 +0900

    Update [프로그래머스] 합승택시.py

--- [프로그래머스] 합승택시.py
+++ [프로그래머스] 합승택시.py
@@ -46,3 +46,41 @@ def solution(n, s, a, b, fares):
         cost = min(cost,dijkstra(s,i)+dijkstra(i,a)+dijkstra(i,b))
         
     return cost
+
+
+
+
+
+
+
+
+
+
+#참고할만한 풀이 
+-----------------------------------------------------
+
+
+from collections import defaultdict
+import heapq
+
+def solution(n, s, a, b, fares):
+    dic = defaultdict(list)
+    for st, ed, co in fares:
+        dic[st].append((co, ed))
+        dic[ed].append((co, st))
+    ans = []
+    for i in range(1, n+1):
+        Q = [(0, i)]
+        visited = [True] * (n+1)
+        dp = [float('inf')] * (n+1)
+        dp[i] = 0
+        while Q:
+            co, des = heapq.heappop(Q)
+            if visited[des]:
+                visited[des] = False
+                for cost, destination in dic[des]:
+                    dp[destination] = min(cost + dp[des], dp[destination])
+                    heapq.heappush(Q, (dp[destination], destination))
+        ans.append(dp[a] + dp[b] + dp[s])
+
+    return min(ans)
