880a9b1ad994d4f12ba7155b395432feba6043e9
yurim cho <45115557+yurim022@users.noreply.github.com>
2021-08-22 19:19:13 +0900

    Update [프로그래머스] 파일명 정렬.py

--- [프로그래머스] 파일명 정렬.py
+++ [프로그래머스] 파일명 정렬.py
@@ -1,3 +1,5 @@
+##첫번째 풀이
+
 import re
 
 def solution(files):
@@ -7,3 +9,13 @@ def solution(files):
                x[0]))
     answer = [ e for i,e in l]
     return answer
+
+
+##두번째 풀이 불필요한 enumerate 과정과, 원래 순서를 유지하는 쏘팅 제외
+import re
+
+def solution(files):
+    files.sort(key = lambda file:(re.findall('^[a-zA-Z]{1}[a-zA-z .-]*',file)[0].lower(),
+               re.findall('[0-9]+',file)[0].zfill(5)))
+    return files
+
