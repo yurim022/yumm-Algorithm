e48a8049f9380dcad6e1c5361ac71e82911d5936
yurim cho <45115557+yurim022@users.noreply.github.com>
2021-08-25 20:58:51 +0900

    Create [프로그래머스] 표 편집.java

---
+++ [프로그래머스] 표 편집.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int n, int k, String[] cmd) {
+        Stack<Integer> remove = new Stack<>();
+        int table_size = n;
+        
+        for(int i = 0; i < cmd.length; ++i){
+            char c = cmd[i].charAt(0);
+        
+
+            if(c == 'U'){
+                k -= Integer.valueOf(cmd[i].substring(2));
+            }else if(c == 'D'){
+                k += Integer.valueOf(cmd[i].substring(2));
+            }else if(c == 'C'){
+                remove.push(k);
+                table_size -= 1;
+                if(k == table_size) k -= 1;
+            }else{
+                int r = remove.pop();
+                table_size += 1;
+                if(k >= r) k+= 1;
+            }
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i< table_size; ++i){
+            sb.append('O');
+        }
+        
+        while( !remove.isEmpty()){
+            sb.insert(remove.pop().intValue(),'X');
+        }
+        return sb.toString();
+    }
+}
