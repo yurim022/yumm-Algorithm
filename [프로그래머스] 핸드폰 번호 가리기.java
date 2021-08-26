7836a0364e152e708aebd0f8dfbaf74cdc23813b
yurim cho <45115557+yurim022@users.noreply.github.com>
2021-08-26 23:06:51 +0900

    Update [프로그래머스] 핸드폰 번호 가리기.java

--- [프로그래머스] 핸드폰 번호 가리기.java
+++ [프로그래머스] 핸드폰 번호 가리기.java
@@ -6,9 +6,22 @@ class Solution {
     }
 }
 
+
+--------
+    
+    class Solution {
+    public String solution(String phone_number) {
+         char[] ch = phone_number.toCharArray();
+        for(int i = 0; i < ch.length - 4; i ++){
+             ch[i] = '*';
+        }
+        return String.valueOf(ch);
+    }
+}
+
 --------
   
-  //밑에꺼가 정규식이긴 한데 위에 풀이가 10배정도 빠름..!!
+  //밑에꺼가 정규식이긴 한데 위에 풀이 2개가 10배정도 빠름..!!
   
   class Solution {
     public String solution(String phone_number) {
