ee793612ed3bf20708e88e792181f0685b16071a
yurim cho <45115557+yurim022@users.noreply.github.com>
2022-05-09 21:04:10 +0900

    Update and rename [프로그래머스] (RE) 더 맵게.python to 더 맵게.py

---
+++ 더 맵게.py
@@ -0,0 +1,32 @@
+import heapq
+
+def solution(scoville, K): #priority que 사용
+    cnt = 0
+    heapq.heapify(scoville)
+    while scoville[0] < K:
+        try:
+            heapq.heappush(scoville,heapq.heappop(scoville)+2*heapq.heappop(scoville))
+            cnt += 1
+        except IndexError:
+            return -1
+    return cnt
+
+
+----------------------------------------------------------------
+
+# 2022-05-09 다시품
+
+import heapq
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    cnt = 0
+    while scoville[0] < K:
+        if len(scoville) <2 :
+            return -1
+        s1 = heapq.heappop(scoville)
+        s2 = heapq.heappop(scoville)
+        heapq.heappush(scoville, s1 + 2*s2)
+        cnt += 1
+    
+    return cnt
