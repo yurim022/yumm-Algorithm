bf37ff120db596932a1e90d33e6a511d27e105ca
yurim cho <45115557+yurim022@users.noreply.github.com>
2022-05-16 23:27:25 +0900

    Update 신고 결과 받기.py

--- 신고 결과 받기.py
+++ 신고 결과 받기.py
@@ -23,3 +23,20 @@ def solution(id_list, report, k):
                 cnt += 1
         answer.append(cnt)
     return answer
+
+
+------
+# 인상깊은 풀이
+
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)
+    reports = { x: 0 for x in id_list}
+    
+    for r in set(report): # 아예 report 자체를 set...!
+        reports[r.split()[1]] += 1
+        
+    for r in set(report):
+        if reports[r.split()[1]] >= k:
+            answer[id_list.index(r.split()[0])] += 1 # index 함수를 적절하게 
+
+    return answer
