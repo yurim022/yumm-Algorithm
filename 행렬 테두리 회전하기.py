3ef883c708caa190316cd541c83b417ffeb50b36
yurim cho <45115557+yurim022@users.noreply.github.com>
2022-05-17 14:30:39 +0900

    Update 행렬 테두리 회전하기.py

--- 행렬 테두리 회전하기.py
+++ 행렬 테두리 회전하기.py
@@ -29,3 +29,46 @@ def solution(rows, columns, queries):
         answer.append(min(changed))
         
     return answer
+
+
+--------
+  
+    # 괜찮았던 풀이 -> 시간복잡도 이게 더 굿. 
+    # map을 두번 초기화 하지 않았고, 위에는 while문에 매번 if문 체크와 pre/cur, changed가 반복 연산되던것을 스택을 통해 한번에 연산했다. 
+    
+    def solution(rows, columns, queries):
+    answer = []
+
+    board = [[i+(j)*columns for i in range(1,columns+1)] for j in range(rows)]
+    # print(board)
+
+    for a,b,c,d in queries:
+        stack = []
+        r1, c1, r2, c2 = a-1, b-1, c-1, d-1
+
+
+        for i in range(c1, c2+1):
+
+            stack.append(board[r1][i])
+            if len(stack) == 1:
+                continue
+            else:
+                board[r1][i] = stack[-2]
+
+
+        for j in range(r1+1, r2+1):
+            stack.append(board[j][i])
+            board[j][i] = stack[-2]
+
+        for k in range(c2-1, c1-1, -1):
+            stack.append(board[j][k])
+            board[j][k] = stack[-2]
+
+        for l in range(r2-1, r1-1, -1):
+            stack.append(board[l][k])
+            board[l][k] = stack[-2]
+
+        answer.append(min(stack))
+
+
+    return answer
