ba7c376ca9534c31f102e0849cff7dccfd5e21a2
yurim cho <45115557+yurim022@users.noreply.github.com>
2022-05-18 01:51:56 +0900

    Create 다단계 칫솔 판매.py

---
+++ 다단계 칫솔 판매.py
@@ -0,0 +1,16 @@
+def solution(enroll, referral, seller, amount):
+    answer = [0] * len(enroll)
+    idx = { e : i for i , e in enumerate(enroll)}
+    
+    for s, a in zip(seller,amount):
+        cur = s
+        money = a * 100
+        while cur != '-':
+            if money == 0: #
+                break
+            tip = money // 10
+            answer[idx[cur]] += (money - tip)
+            money = tip
+            cur = referral[idx[cur]]
+        
+    return answer
