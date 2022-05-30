6b919d8556aea65eb43620e4be5afd0a53ab30cd
yurim cho <45115557+yurim022@users.noreply.github.com>
2022-05-30 15:05:46 +0900

    Update [백준] N과 M(1).py

--- [백준] N과 M(1).py
+++ [백준] N과 M(1).py
@@ -22,3 +22,26 @@ def backtracking(j,k):
         if i not in visited:
             backtracking(i,k+1)
     visited.pop()
+
+    
+    -----------------------
+    
+
+# 다른분 풀이. 훨씬 깔끔하게 풀 수 있었다...
+
+n,m = list(map(int,input().split()))
+ 
+s = []
+ 
+def dfs():
+    if len(s)==m:
+        print(' '.join(map(str,s)))
+        return
+    
+    for i in range(1,n+1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+ 
+dfs()
