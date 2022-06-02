b59f8a1e222e2f7b8622cdb89dcdb558c6ff33a6
yurim cho <45115557+yurim022@users.noreply.github.com>
2022-06-02 21:08:52 +0900

    Update [백준] NQUEEN.py

--- [백준] NQUEEN.py
+++ [백준] NQUEEN.py
@@ -44,4 +44,30 @@ if __name__=='__main__':
     #다른 코드 -> 이게 더 효율적이당
     
     
-    
+    n = int(input())
+
+ans = 0
+row = [0] * n
+
+def is_promising(x):
+    for i in range(x):
+        if row[x] == row[i] or abs(row[x] - row[i]) == (x -i):
+            return False
+
+    return True
+
+
+def n_queens(x):
+    global ans
+    if x == n:
+        ans += 1
+        return
+
+    for i in range(n):
+        row[x] = i
+        if is_promising(x):
+            n_queens(x+1)
+
+if __name__=='__main__':
+    n_queens(0)
+    print(ans)
